b3cbdb4722ecafbb818e045a3ea335e2c73c9f4b
michitaro.koike <michitaro.nike@gmail.com>
2018-07-17 10:06:04 +0900

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ class KeyCombination {
     shift = false
     ctrl = false
     meta = false
-    key: string
+    key: PhysicalKey
 
     constructor(source: string) {
         const MODIFIERS = ['alt', 'shift', 'ctrl', 'meta']
@@ -18,28 +18,57 @@ class KeyCombination {
         if (keys.length != 1)
             throw new Error(`invalid keybind source: ${source}`)
         const key = keys[0]
-        this.key = key.length == 1 ? key.toLowerCase() : key
+        this.key = PhysicalKey.fromString(key)
     }
 
     match(e: KeyboardEvent) {
-        const key = e.key.length == 1 ? e.key.toLowerCase() : e.key
         return (
             e.altKey == this.alt &&
             e.shiftKey == this.shift &&
             e.ctrlKey == this.ctrl &&
             e.metaKey == this.meta &&
-            key == this.key)
+            this.key.equals(PhysicalKey.fromEvent(e)))
     }
 
     html() {
-        const key = this.key.length == 1 ? this.key.toUpperCase() : this.key
         return [
             this.alt ? '&#x2325;' : '',
             this.shift ? '&#x21e7;' : '',
             this.ctrl ? '&#x2303;' : '',
             this.meta ? '&#x2318;' : '',
-            key
+            this.key.toString()
         ].join('')
+    }
+}
+
+
+class PhysicalKey {
+    private constructor(private keyCode: number) { }
+
+    static fromEvent(e: KeyboardEvent) {
+        return new PhysicalKey(e.keyCode)
+    }
+
+    static fromString(s: string) {
+        if (s.length == 1) {
+            return new PhysicalKey(s.toUpperCase().charCodeAt(0))
+        }
+        else {
+            const code = name2code[s]
+            if (code == undefined)
+                throw new Error(`unknwon key name: ${s}`)
+            else
+                return new PhysicalKey(code)
+        }
+    }
+
+    equals(o: PhysicalKey) {
+        return this.keyCode == o.keyCode
+    }
+
+    toString() {
+        const name = code2name[this.keyCode]
+        return name || String.fromCharCode(this.keyCode)
     }
 }
 
@@ -53,11 +82,12 @@ const keybinds: Keybind[] = []
 document.addEventListener('keydown', e => {
     if (e.target != document.body)
         return
-    for (const kb of keybinds)
+    for (const kb of keybinds) {
         if (kb.kc.match(e)) {
             e.preventDefault()
             kb.handler(e)
         }
+    }
 })
 
 
@@ -75,3 +105,27 @@ export function on(source: string, handler: (e: KeyboardEvent) => void) {
 export function html(source: string) {
     return (new KeyCombination(source)).html()
 }
+
+
+const name2code = {
+    Backspace: 8,
+    Tab: 9,
+    Enter: 13,
+    Shift: 16,
+    Ctrl: 17,
+    Escape: 27,
+    Space: 32,
+    Delete: 46,
+    Left: 37,
+    Up: 38,
+    Right: 39,
+    Down: 40,
+}
+
+const code2name = (() => {
+    const d: { [code: number]: string } = {}
+    for (const name in name2code) {
+        d[name2code[name]] = name
+    }
+    return d
+})()
